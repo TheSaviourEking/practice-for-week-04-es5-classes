d261ca6b15b4d80f2a174ebeb218413bddb553bc
TheSaviourEking <saviour.eking@outlook.com>
2023-09-24 03:00:09 +0100

    SOLVED: fix classes/array.js to pass test

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,12 @@
 // Your code here
+Array.prototype.isEqual = function(arr) {
+    if(this.length === arr.length) {
+        for(let i = 0; i < this.length; i++) {
+            if (this[i] !== arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
